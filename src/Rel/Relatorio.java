a15366f2ce9411979b540fd4e64e33f7e50592d0
Wellinton03 <wellintonhf123@gmail.com>
2024-01-04 15:44:42 -0300

    alteração na classe relátorio

---
+++ src/Rel/Relatorio.java
@@ -0,0 +1,26 @@
+package Rel;
+
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class Relatorio {
+	
+	String getNome;
+	String getDescricaoProblema;
+	LocalDate getData;
+
+	
+	public static void main(String[] args) {
+		Relatorio relatorio = new Relatorio();
+		Scanner scan = new Scanner(System.in);
+		
+		
+		String getNome = scan.nextLine();
+		
+	}
+	
+	
+	
+	
+
+}
--- src/Rel/Relátorio.java
+++
@@ -1,5 +0,0 @@
-package Rel;
-
-public class Relátorio {
-
-}
