f001b0c72e6326e92f5761fb8eb8d5bcfb9d83f3
Wellinton Silveira <105024900+Wellinton03@users.noreply.github.com>
2024-01-05 15:54:36 -0300

    Create Funcionario.java

---
+++ src/Rel/Funcionario.java
@@ -0,0 +1,21 @@
+package Rel;
+
+public class Funcionario {
+	private String email;
+	private String dsNome;
+	private Integer idFuncionario;
+	
+	public Integer getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(Integer idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public String getDsNome() {
+		return dsNome;
+	}
+	public void setDsNome(String dsNome) {
+		this.dsNome = dsNome;
+	}
+
+}
